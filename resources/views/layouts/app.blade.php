c255757ac9cd9ad53ce0b3c4e9cbf902b2cea823
Amariah <rigodonamariah16@gmail.com>
2022-11-02 16:45:48 +0000

    modified navigation bar. added drop down menu with links to other pages. added an icon that goes to homepage when user clicks it

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -20,8 +21,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
+                <!--can change to logo-->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                 <i class="bi bi-house-fill"></i> 
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -30,8 +32,23 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                    
+                    <li class="nav-item dropdown">
+                    <div class="dropdown"> 
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+                        Dropdown</button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="{{ url('/women') }}">Shop Women's Clothing</a></li>
+                            <div class="dropdown-divider"></div>
+                            <li><a class="dropdown-item" href="{{ url('/men') }}">Shop Men's Clothing</a></li>
+                            <div class="dropdown-divider"></div>
+                            <li><a class="dropdown-item" href="{{ url('/contact') }}">Contact us</a></li>
+                            <div class="dropdown-divider"></div>
+                            <li><a class="dropdown-item" href="{{ url('/about') }}">About us</a></li>
+                        </ul>
+                     </div>
 
-                    </ul>
+                    
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
