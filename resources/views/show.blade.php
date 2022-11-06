33e2926256bf3401d30600b8f02d12d04f50b9fc
Jasraj Singh <elijahbaggins3@gmail.com>
2022-11-06 21:21:41 +0000

    Created a database seeder. Created a product factory to generate fake data. Added basic cards to test fake data, layout doesn't look good, but the data has been created and fetched successfully.

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,13 +5,24 @@
 <div class="container ">
     <div class="row">
 
-        @foreach ($products as $product)
-        <div class="col">
-            <h1>{{$product -> title}}</h1>
-            <h3>{{$product -> description}}</h3>
-            <p>Price: {{$product -> price}}</p>
+        <div class="card-group">
+            @foreach ($products as $product)
+            {{-- <div class="col">
+                <h1>{{$product -> title}}</h1>
+                <h3>{{$product -> description}}</h3>
+                <p>Price: {{$product -> price}}</p>
+            </div> --}}
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="{{$product -> image}}" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">{{$product-> title}}</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            
+            @endforeach
         </div>
-        @endforeach
 
     </div>
 </div>
