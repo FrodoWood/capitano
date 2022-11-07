34fee63932bc07c0b73be94bc78e0e05f8969678
Jasraj Singh <elijahbaggins3@gmail.com>
2022-11-07 23:03:15 +0000

    Added role column to users table

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,25 +2,25 @@
 
 @section('content')
 
-<div class="container">
-    <div class="card-group">
-            <div class="row">
-                @foreach ($products as $product)
-                    <div class="col-4">
-                        <div class="card mb-5" style="width: 18rem;">
-                            <img class="card-img-top" src="{{$product -> image}}" alt="Card image cap">
-                            <div class="card-body">
-                              <h5 class="card-title">{{$product-> title}}</h5>
-                              <p class="card-text">{{$product-> description}}</p>
-                              <p class="card-text">£{{$product-> price}}</p>
-                              <a href="#" class="btn btn-success">BUY</a>
-                            </div>
-                          </div>
-                    </div>
-                
-                @endforeach
+    <div class="container center">
+        <div class="card-group">
+                <div class="row">
+                    @foreach ($products as $product)
+                        <div class="col-4">
+                            <div class="card mb-5" style="width: 18rem;">
+                                <img class="card-img-top" src="{{$product -> image}}" alt="Card image cap">
+                                <div class="card-body">
+                                  <h5 class="card-title">{{$product-> title}}</h5>
+                                  <p class="card-text">{{$product-> description}}</p>
+                                  <p class="card-text">£{{$product-> price}}</p>
+                                  <a href="#" class="btn btn-success">BUY</a>
+                                </div>
+                              </div>
+                        </div>
+    
+                    @endforeach
+                </div>
             </div>
-        </div>
-</div>
+    </div>
 
 @endsection
