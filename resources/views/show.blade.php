f1d2a2cfc8fa74133fd386e3383547ab94b70807
Jasraj Singh <elijahbaggins3@gmail.com>
2022-11-07 13:17:48 +0000

    Improved card layout for testing

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,29 +2,25 @@
 
 @section('content')
 
-<div class="container ">
-    <div class="row">
-
-        <div class="card-group">
-            @foreach ($products as $product)
-            {{-- <div class="col">
-                <h1>{{$product -> title}}</h1>
-                <h3>{{$product -> description}}</h3>
-                <p>Price: {{$product -> price}}</p>
-            </div> --}}
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="{{$product -> image}}" alt="Card image cap">
-                    <div class="card-body">
-                      <h5 class="card-title">{{$product-> title}}</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="container">
+    <div class="card-group">
+            <div class="row">
+                @foreach ($products as $product)
+                    <div class="col-4">
+                        <div class="card mb-5" style="width: 18rem;">
+                            <img class="card-img-top" src="{{$product -> image}}" alt="Card image cap">
+                            <div class="card-body">
+                              <h5 class="card-title">{{$product-> title}}</h5>
+                              <p class="card-text">{{$product-> description}}</p>
+                              <p class="card-text">£{{$product-> price}}</p>
+                              <a href="#" class="btn btn-success">BUY</a>
+                            </div>
+                          </div>
                     </div>
-                  </div>
-            
-            @endforeach
+                
+                @endforeach
+            </div>
         </div>
-
-    </div>
 </div>
 
 @endsection
