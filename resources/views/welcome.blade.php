855c24e1813d2e06143081375e84b1668973709a
Amariah <rigodonamariah16@gmail.com>
2022-11-08 14:52:18 +0000

    got both  previous and next buttons to work when scrolling through carousel added background colour to products on sale, otherwise buttons are not visible

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,32 +11,41 @@
 
 <body>
 
-<section class="ProductsOnSale">
+<section id="ProductsOnSale" class="p-3 mb-2 bg-danger text-white">
 
 <h2>Products on Sale</h2> 
 
 
 <div  id="carouselProducts"  class="carousel slide">
 <div class="carousel-inner">
+
  <div class="carousel-item active">
-  <img class="img-thumbnail" src="{{url('images/sale.jpg')}}" alt="On Sale" style= "width: 90%; height:350px">
+ <div class="text-center">
+  <img class="img-thumbnail" src="{{url('images/sale.jpg')}}" alt="On Sale" style= "width: 50%; height:350px">
   <div class="text">Hurry up and grab our products at a discounted price! For a limited time only. 
       Offering quality at an affordable price. Browse through to see our amazing collection </div>
 </div>
+ </div>
 
   <div class="carousel-item">
+  <div class="text-center">
     <img class="img-thumbnail" src="{{URL('images/productWomen.jpg')}}" alt="On Sale" style= "width: 50%; height:450px">
     <div class="text">Women's matching sports bra and leggins</div>
   </div>
+</div>
 
   <div class="carousel-item">
+  <div class="text-center">
     <img class="img-thumbnail" src="{{URL('images/productMen.jpg')}}" alt="On Sale" style= "width: 50%; height:450px">
     <div class="text">Men's Joggers</div>
   </div>
+</div>
 
   <div class="carousel-item">
+  <div class="text-center">
     <img class="img-thumbnail" src="{{URL('images/productWomen1.jpg')}}" alt="On Sale" style= "width: 50%; height:450px">
     <div class="text">Women's crop top and leggins</div>
+  </div>
   </div>
 
  
@@ -52,13 +61,13 @@ idea: change previous to scroll through. there'll b only one button-->
 <button class="carousel-control-prev" type="button" role="button" data-bs-target="#carouselProducts"
             data-bs-slide="prev" >
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
+            <span class="sr-only"></span>
         </button> 
         
         <button class="carousel-control-next" type="button" data-bs-target="#carouselProducts"
             data-bs-slide="next" >
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
+            <span class="visually-hidden"></span>
         </button> 
 
 
@@ -130,15 +139,27 @@ function displaySlides(n) {
 
 <br><br>
 
+
 <section class="companyNews">
-<div class="container">
+  <h2>Company News</h2>
+<div class="container-fluid">
   <div class="row">
-   <div class="col-sm">
-     <h2>New Launch</h2>
+   <div class="col-md-6">
+     <h3 class="text-center">New Launch</h3>
      <p>Capitano Clothing launches a brand new collection of leggins in collaboration with award winning designer Regina Geller.</p>
+     
+     <!--
+     <div class = "vertical" style="  border-left: 6px solid black;
+            height: 200px;
+            position:absolute;
+            top:1000px;
+            left: 50%;
+            display: inline-block" ></div> -->
+
+
    </div>
-   <div class="col-sm">
-     <h2>Giving back to the community</h2>
+   <div class="col-md-6">
+     <h3 class="text-center">Giving back to the community</h3>
      <p>To celebrate its four year anniversary since opening, capitano clothing orgarnised a charity fair. The company raised £100 thousand pounds 
       for the Youth Sport Trust which is the UK's leading charity, improving every young person's education and development through sport and play.</p>
    </div>
@@ -156,16 +177,8 @@ function displaySlides(n) {
 <div style="position: absolute;top: 70px;left: 60px;color:white; font-size:30px">Always providing the best!</div>
 <button type="button" class="btn btn-light"  style="position: absolute;top: 130px;left: 90px;"> <a href="/men">Shop Men</a></button>
 <button type="button" class="btn btn-light"  style="position: absolute;top: 130px;left: 200px;"> <a href="/women">Shop Women</a></button>
-<!-- Delete before submitting
-  reason y I put those href: web.php line 33 + 34 -->
 </section>
 
-<!--
-<footer>
- 
-  <a href=" https://www.instagram.com/capitano.clothing_/"> Instagram</a>
-  <a href="//twitter.com/CapitanoCloth"> Twitter </a>
-   <img src="{{URL('images/copyright.jpg')}}"  alt="copyright" style="width:150px" /> 
-</footer> -->
+
 
 @endsection
