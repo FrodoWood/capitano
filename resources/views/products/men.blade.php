ee4b79cd1ff55a2c70a2d51df08ac0720fff11e9
Jasraj Singh <elijahbaggins3@gmail.com>
2022-11-15 01:14:03 +0000

    Improved product card layout only in men page. Added some utility css classes.

--- resources/views/products/men.blade.php
+++ resources/views/products/men.blade.php
@@ -4,28 +4,34 @@
 
 <h1 class="text-center text-bold text-black-50">Men</h1>
     <div class="container">
-        <div class="card-group justify-content-center">
-                <div class="row">
+        <div class="card-deck card-container">
+                
                     @foreach ($products as $product)
-                        <div class="col-4">
+                        
                             <a href="{{route('showProduct', ['product' => $product])}}">
 
-                                <div class="card mb-5" style="width: 18rem;">
+                                <div class="card product-card mb-5 m-4 ">
                                     <img class="card-img-top" src="{{$product -> image}}" alt="Card image cap">
-                                </a>
+                            </a>
                                     <div class="card-body">
-                                      <h5 class="card-title">{{$product-> title}}</h5>
-                                      <p class="card-text">{{$product-> description}}</p>
-                                      <p class="card-text">£{{$product-> price}}</p>
-                                      <a href="#" class="btn btn-success">Add to Basket</a>
+                                        <h5 class="card-title">{{$product-> title}}</h5>
+                                        <p class="card-text">{{$product-> description}}</p>
                                     </div>
-                                  </div>
-
-                        </div>
-    
-                    @endforeach
+                                    <div class="card-footer mb-2">
+                                        <div class="row align-items-center">
+                                            <div class="col-4">
+                                                <p class="card-text h5 ">£{{$product-> price}}</p>
+                                            </div>
+                                            <div class="col-8 text-align-end">
+                                                <a href="#" class="btn btn-success">Add to Basket</a>
+                                            </div>
+                                        </div>
+                                        
+                                        
+                                    </div>
+                                </div>
+                        @endforeach
                 </div>
             </div>
-    </div>
 
 @endsection
