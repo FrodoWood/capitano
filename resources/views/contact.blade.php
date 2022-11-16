7de2ba05853d1543aeab37f11960f7140eecb6e7
Jasraj Singh <elijahbaggins3@gmail.com>
2022-11-16 02:27:54 +0000

    Fixed navbar links not working on contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,17 +2,6 @@
 
 @section('content')
    
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Get In Touch</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-
 <div class="container mt-3">
   <h2>Get In Touch</h2>
   <form action="/action_page.php">
@@ -32,9 +21,6 @@
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
 </div>
-
-</body>
-</html>
 
 @endsection
 
