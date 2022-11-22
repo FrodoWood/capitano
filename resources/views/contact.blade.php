5261e06eac44fa8a6394e6fdf25a548077e8e73d
Sunny210107681 <210107681@aston.ac.uk>
2022-11-22 14:57:48 +0000

    added comment area

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,27 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<form class="row g-3 needs-validation" novalidate>
-<div class="container mt-4">
-  <h2 class = "h1- responsive text-center mx-auto mb-2">Get In Touch</h2>
-  <p class ="text-center w-responsive mx-auto mb-4"> "Please enter your details below about your query and we will contact you back as soon as possible!</p>
-  <form action="/action_page.php">
-    <div class="mb-4">
-      <label for="name">Name:</label>
-      <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
-      <div class = "invalid-feedback" data-sb-feedback ="name:required">Your name is required.</div>
-    </div>
-    <div class="mb-4">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-    </div>
-    <div class="mb-4">
-      <label for ="address">Address:</label>
-      <input type ="text" class ="form-control" id="address" placeholder="Enter adress">
-    </div>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+<div class="container">
+  <form class="col-xs-6">
+  <div class="container mt-4">
+    <h1 class = "h1- responsive text-center mx-auto mb-2">Get In Touch</h1>
+    <p class ="text-center w-responsive mx-auto mb-4"> Enter your details below about your query and we will contact you back as soon as possible!</p>
+    <form action="/action_page.php">
+      <div class="mb-4">
+        <label for="name">Name:</label>
+        <input type="text" class="form-control" id="name" placeholder="Enter name" name="name" required>
+      </div>
+      <div class="mb-4">
+        <label for="email">Email:</label>
+        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
+      </div>
+      <div class="mb-4">
+        <label for ="text area">Comments:</label>
+        <input type ="text" class ="form-control" id="text area" placeholder="Please enter reason for contacting us" required>
+      </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Send</button>
+    </form>
+  </div>
 </div>
 
 @endsection
