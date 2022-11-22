74f41407b0af0c8fa3dc40bbb09c1c5eca031652
Sunny210107681 <210107681@aston.ac.uk>
2022-11-22 12:53:01 +0000

    Added email

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-   
-<div class="container mt-3">
-  <h2>Get In Touch</h2>
+<form class="row g-3 needs-validation" novalidate>
+<div class="container mt-4">
+  <h2 class = "h1- responsive text-center mx-auto mb-2">Get In Touch</h2>
+  <p class ="text-center w-responsive mx-auto mb-4"> "Please enter your details below about your query and we will contact you back as soon as possible!</p>
   <form action="/action_page.php">
-    <div class="mb-3 mt-3">
+    <div class="mb-4">
+      <label for="name">Name:</label>
+      <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
+      <div class = "invalid-feedback" data-sb-feedback ="name:required">Your name is required.</div>
+    </div>
+    <div class="mb-4">
       <label for="email">Email:</label>
       <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
     </div>
-    <div class="mb-3">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+    <div class="mb-4">
+      <label for ="address">Address:</label>
+      <input type ="text" class ="form-control" id="address" placeholder="Enter adress">
     </div>
-    <div class="form-check mb-3">
-      <label class="form-check-label">
-        <input class="form-check-input" type="checkbox" name="remember"> Remember me
-      </label>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
