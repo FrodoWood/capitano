60b2480b037c8c2bdfd4ba5e63e25066ee40ce38
Sunny210107681 <210107681@aston.ac.uk>
2022-11-24 15:21:56 +0000

    Added dropdown

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-  <form class="container">
+  <form class="container form-control center">
   <div class="container form-control-lg  mt-4">
     <h1 class = "h1- responsive text-center mx-auto">Get In Touch</h1>
     <p class ="text-center responsive mx-auto mb-4"> Enter your details below about your query and we will contact you back as soon as possible!</p>
@@ -22,6 +22,14 @@
       <div class="mb-4">
         <label for ="text area">Comments:</label>
         <textarea class ="form-control" id="text area" rows= "3"placeholder="Please enter reason for contacting us" required></textarea>
+      </div>
+      <div class ="mb-4">
+        <label>How would you like us to contact you?</label>
+        <select name ="contact" class ="form-control" id="dropdown" required>
+          <option disabled selected="">Choose option</option>
+          <option>Email</option>
+          <option>Phone</option>
+        </select>
       </div>
       </div>
       <div class ="container">
