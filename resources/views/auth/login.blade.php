2100c378cbe69ec67fe7766d0aa8a8feaa323fc0
Jasraj Singh <elijahbaggins3@gmail.com>
2022-11-28 22:44:54 +0000

    Made login responsive

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,40 +2,71 @@
 
 @section('content')
 <div class="container">
-  <div class="row">
-    <div class="col-8" style="padding:30px;margin-left:17%;padding-bottom:0px;">
-    <div class="mb-4" style="float:left">
-    <div class="img" > <img src="{{ URL('images/image_6487327.JPG')}}" height="400"/> </div>
-  </div>
-  <div class="mb-5 " style="float:left; margin-left:50px;">
-    <h3 class="text-center">{{ __('Login') }}</h3>
-    <div class=" ">
-      <p class="social-media d-flex justify-content-end"> <a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a> <a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a> </p>
-    </div>
-    <form method="POST" action="{{ route('login') }}">
-      @csrf
-      <div class="form-group">
-        <label for="email" class="">{{ __('Email Address') }}</label>
-        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-        @error('email') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror </div>
-      <div class="form-group">
-        <label for="password" class="col-md-4 ">{{ __('Password') }}</label>
-        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-        @error('password') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror </div>
-      <div class="form-group">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-          <label class="form-check-label" for="remember"> {{ __('Remember Me') }} </label>
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card rounded-0">
+                
+                <div class="card-body p-0">
+                    <div class="row">
+                      <div class=" col-sm-6 col-md-6 col-lg-6 p-4 pe-5">
+                        <form method="POST" action="{{ route('login') }}">
+                          @csrf
+                          <div class="row mb-3">
+                              <label for="email" class="col-lg-12 form-label ">{{ __('Email Address') }}</label>
+                              <div class="col-lg-12">
+                                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                  @error('email')
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+                          <div class="row mb-3">
+                              <label for="password" class="col-lg-12 form-label ">{{ __('Password') }}</label>
+                              <div class="col-lg-12">
+                                  <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                  @error('password')
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+                          <div class="row mb-3">
+                              <div class="col-md-6 ">
+                                  <div class="form-check">
+                                      <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                      <label class="form-check-label" for="remember">
+                                          {{ __('Remember Me') }}
+                                      </label>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="row mb-0">
+                              <div class="col-lg-12 ">
+                                  <button type="submit" class="btn btn-primary">
+                                      {{ __('Login') }}
+                                  </button>
+                                  @if (Route::has('password.request'))
+                                      <a class="btn btn-link" href="{{ route('password.request') }}">
+                                          {{ __('Forgot Your Password?') }}
+                                      </a>
+                                  @endif
+                              </div>
+                          </div>
+                      </form>
+                      </div>
+                      
+                      <div class=" col-sm-6 col-md-6 col-lg-6 order-first">
+                          <div class="row" > <img class="img-fluid" src="{{ URL('images/image_6487327.JPG')}}" /> </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="form-group">
-        <button type="submit" class="btn btn-primary"> {{ __('Login') }} </button>
-      </div>
-      <div class="form-group" style="margin-top:130px;"> @if (Route::has('password.request')) <a class="btn btn-link" href="{{ route('password.request') }}"> {{ __('Forgot Your Password?') }} </a> @endif
-        Not a member? <a data-toggle="tab" href="#signup">Sign Up</a> </div>
-    </form>
-  </div>
+    </div>
 </div>
-</div>
-</div>
-@endsection 
+@endsection
+
+
+
+
