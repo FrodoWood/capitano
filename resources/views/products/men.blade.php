c993a75ce579574536fd5ca33202129d029a5404
sajni4 <114992911+sajni4@users.noreply.github.com>
2022-11-29 11:47:52 +0000

    added carousel to mens page

--- resources/views/products/men.blade.php
+++ resources/views/products/men.blade.php
@@ -2,11 +2,35 @@
 
 @section('content')
 
-<h1 class="text-center text-bold text-black-50">Men</h1>
+<h1 class="text-center text-bold text-black-50">Men's</h1><br><br><br><br>
 
 <!-- add stuff here -->
-<h2> Shop today </h2>
-<p> hello</p>
+
+<div id="banner-carousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+
+      <div class="carousel-item">
+        <img src="{{URL('images/banner/woman_yoga_wide.jpg')}}" class="d-block w-100 carousel-banner-img" alt="">
+      </div>
+      <div class="carousel-item">
+        <img src="{{URL('images/banner/woman_stretching_wide.jpg')}}" class="d-block w-100 carousel-banner-img" alt="">
+      </div>
+      <div class="carousel-item active">
+        <img src="{{URL('images/banner/woman_sitting_wide.jpg')}}" class="d-block w-100 carousel-banner-img" alt="">
+      </div>
+
+      </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#banner-carousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#banner-carousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+
 
 
     <div class="container">
