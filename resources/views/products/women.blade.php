4d8aa8f520b805abb6377c4a285e5d61f2fd537d
sajni4 <114992911+sajni4@users.noreply.github.com>
2022-11-29 11:49:34 +0000

    added carousel to women's page

--- resources/views/products/women.blade.php
+++ resources/views/products/women.blade.php
@@ -7,6 +7,31 @@
 <!-- add stuff here -->
 
 
+<div id="banner-carousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+
+      <div class="carousel-item">
+        <img src="{{URL('images/banner/woman_yoga_wide.jpg')}}" class="d-block w-100 carousel-banner-img" alt="">
+      </div>
+      <div class="carousel-item">
+        <img src="{{URL('images/banner/woman_stretching_wide.jpg')}}" class="d-block w-100 carousel-banner-img" alt="">
+      </div>
+      <div class="carousel-item active">
+        <img src="{{URL('images/banner/woman_sitting_wide.jpg')}}" class="d-block w-100 carousel-banner-img" alt="">
+      </div>
+
+      </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#banner-carousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#banner-carousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+
     <div class="container">
         <div class="card-deck card-container">
                 
