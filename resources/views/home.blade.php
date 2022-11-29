69c2e00090172bd5ee4c494b4ab250a02e9885dd
Jasraj Singh <elijahbaggins3@gmail.com>
2022-11-29 04:16:28 +0000

    Added content to homepage based on Amariah's page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,41 +84,105 @@
   </div>
 
 {{-- Products section --}}
-<div class="container">
+<div class="container-fluid w-75 px-0">
 
 
-    <div class="card-deck card-container">
-            
+    <h2 class="text-bold text-center font-weight-bold">Trending Now</h2>
+    <div class="card-deck card-container justify-content-between">
                 @foreach ($womenProducts as $womenProduct)
-                    
-                
-                <div class="card product-card mb-5 m-4 rounded-0">
-                                <a href="{{route('showProduct', ['product' => $womenProduct])}}">
-                                <img class="card-img-top rounded-0" src="{{$womenProduct -> image}}" alt="Card image cap">
-                                </a>
-                                <div class="card-body">
-                                    <h5 class="card-title">{{$womenProduct-> title}}</h5>
-                                    <p class="card-text">{{$womenProduct-> description}}</p>
-                                </div>
-                                <div class="card-footer mb-2">
-                                    <div class="row align-items-center">
-                                        <div class="col-4">
-                                            <p class="card-text h5 ">£{{$womenProduct-> price}}</p>
-                                        </div>
-                                        <div class="col-8 btn-group">
-                                            
-                                            <button class="add-to-cart btn btn-outline-dark rounded-0" type="button" class="btn btn-sm btn-outline-secondary" 
-                                                    data-id="{{$womenProduct->id}}" data-name="{{$womenProduct->title}}" data-price="{{$womenProduct->price}}">Add to Cart</button>
-                                        </div>
-                                    </div>
-                                    
-                                    
-                                </div>
+                @if($loop->index >= 4)
+                    @break
+                @else
+                <div class="card product-card my-4 rounded-0 border-0">
+                    <a href="{{route('showProduct', ['product' => $womenProduct])}}">
+                    <img class="card-img-top rounded-0" src="{{$womenProduct -> image}}" alt="Card image cap">
+                    </a>
+                    <div class="card-body p-0 pt-3">
+                        <h5 class="card-title text-center">{{$womenProduct-> title}}</h5>
+                        
                     </div>
-                    @endforeach
+                    <div class="card-footer mb-2 text-center">
+                        <div class="row align-items-center">
+                            <div class="col-12 mb-3">
+                                <p class="card-text h5 ">£{{$womenProduct-> price}}</p>
+                            </div>
+                            <div class="col-12 ">
+                                <button class="add-to-cart btn btn-outline-dark rounded-0" type="button" class="btn btn-sm btn-outline-secondary" 
+                                        data-id="{{$womenProduct->id}}" data-name="{{$womenProduct->title}}" data-price="{{$womenProduct->price}}">Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endif
+                
+                @endforeach
                     
             </div>
+</div>
+
+{{-- Banner --}}
+<div class="container-fluid w-75 bg-danger my-5">
+    <div class="row">
+        <div class="col-12 p-0 m-0">
+            <a href="{{url('/women')}}"><img class="img w-100 banner-img" src="{{url('images/sale.jpg')}}" alt="On Sale"></a>
+        </div>
     </div>
+</div>
+
+{{-- More products --}}
+<div class="container-fluid w-75 px-0">
+
+
+    <h2 class="text-bold text-center font-weight-bold">Unmissable Deals</h2>
+    <div class="card-deck card-container justify-content-between">
+                @foreach ($menProducts as $menProduct)
+                @if($loop->index >= 4)
+                    @break
+                @else
+                <div class="card product-card my-4 rounded-0">
+                    <a href="{{route('showProduct', ['product' => $menProduct])}}">
+                    <img class="card-img-top rounded-0" src="{{$menProduct -> image}}" alt="Card image cap">
+                    </a>
+                    <div class="card-body">
+                        <h5 class="card-title">{{$menProduct-> title}}</h5>
+                        <p class="card-text">{{$menProduct-> description}}</p>
+                    </div>
+                    <div class="card-footer mb-2">
+                        <div class="row align-items-center">
+                            <div class="col-4">
+                                <p class="card-text h5 ">£{{$menProduct-> price}}</p>
+                            </div>
+                            <div class="col-8 btn-group">
+                                <button class="add-to-cart btn btn-outline-dark rounded-0" type="button" class="btn btn-sm btn-outline-secondary" 
+                                        data-id="{{$menProduct->id}}" data-name="{{$menProduct->title}}" data-price="{{$menProduct->price}}">Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endif
+                
+                @endforeach
+                    
+            </div>
+</div>
+
+{{-- Men and women selection --}}
+<div class="container-fluid w-75 px-0 my-5">
+    <div class="row text-center text-uppercase h3">
+        <div class="col-6 order-first">Shop women's</div>
+        <div class="col-6">Shop men's</div>
+    </div>
+<div class="row">
+    <div class="col-6 order-first">
+        <a href="{{url('/women')}}"><img class="img w-100 carousel-banner-img" src="{{URL('images/productWomen.jpg')}}" alt="Women" ></a>
+    </div>
+    <div class="col-6">
+        <a href="{{url('/men')}}"><img class="img w-100 carousel-banner-img" src="{{URL('images/productMen.jpg')}}" alt="Men" ></a>
+    </div>
+</div>
+</div>
 
 @endsection
 
