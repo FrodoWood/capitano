1198ef0521a55ee0c44aa5baa9018a72fdc716ca
Jasraj Singh <elijahbaggins3@gmail.com>
2022-11-29 20:12:10 +0000

    Table styling done

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,11 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
+    @php
+        $count = count($orders);
+    @endphp
 
-<div class="container">
-    {{-- <h2>Welcome {{auth()->user()->name}}</h2> --}}
-    <h2 class="text-center text-uppercase">Your Orders</h2>
-    
+{{-- <h2>Welcome {{auth()->user()->name}}</h2> --}}
+<div class="container-fluid w-50 px-0 mt-4">
+    <div class="row">
+        <div class="col-6"><h2 class="text-bold font-weight-bold text-secondary h4">Your Orders</h2></div>
+        <div class="col-6 text-end h5"><span class="">{{$count}}
+            @if ($count > 1 or $count ==0)
+            orders
+            @else
+            order
+            @endif
+        </span></div>
+    </div>
+    <hr class="my-4">
+</div>
+
+<div class="container w-50 mt-5">
     @foreach ($orders as $order)
     @php
         $order_items = $order->order_items;
@@ -15,45 +30,63 @@
 
     @endphp
         <div class="row my-4 justify-content-center">
-            <div class="col-md-10">
-                <div class="card">
+            <div class="col-md-12">
+                <div class="card rounded-0">
                     <div class="card-header"> 
                         <div class="row">
-                            <div class="col-12 px-2 col-md-4 text-secondary text-uppercase">Order placed:
+                            <div class="col-12 px-2 col-lg-5 text-secondary text-uppercase">Order placed:
                                  <span class="text-dark px-1 text-capitalize">{{$converted_date}}</span>
                             </div>
-                            <div class="col-12 px-2 col-md-2 text-secondary text-uppercase">Total
+                            <div class="col-12 px-2 col-lg-2 text-secondary text-uppercase">Total
                                 <span class="text-dark px-1 text-capitalize">{{" £".$order->price}}</span>
                             </div>
-                            <div class="col-12 px-2 col-md-4 text-secondary">DELIVER TO 
+                            <div class="col-12 px-2 col-lg-3 text-secondary">DELIVER TO 
                                 <span class="text-primary h5 px-1">{{$order_address->firstname}}</span> 
                                 <span class="text-primary h5 ">{{$order_address->lastname}}</span>
                             </div>
-                            <div class="col-12 px-2 col-md-2 text-end text-uppercase">Order #: {{$order->id}}</div>
+                            <div class="col-12 px-2 col-lg-2 text-end text-uppercase">Order #: {{$order->id}}</div>
                         </div>
                     </div>
                     <ul class="list-group list-group-flush">
                         {{-- $item is the array of order items stored as json inside the database --}}
                         <li class="list-group-item">
-                            <table class="table">
-                                <thead class="thead-dark">
-                                    <tr>
-                                        <th scope="col">Product</th>
-                                        <th scope="col">Price</th>
-                                        <th scope="col">Quantity</th>
-                                    </tr>
-                                </thead>
-                            
-                                <tbody>
-                                    @foreach ($order_items as $item)
-                                        <tr>
-                                            <td>{{$item['name']}}</td>
-                                            <td>{{$item['price']}}</td>
-                                            <td>{{$item['qty']}}</td>
+                            <div class="table-responsive">
+                                <table class="table table-hover">
+                                    <thead class=" ">
+                                        <tr class="">
+                                            <th scope="col">Product</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Price</th>
+                                            <th scope="col">Quantity</th>
                                         </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                    </thead>
+                                
+                                    <tbody>
+                                        @foreach ($order_items as $item)
+                                        @php
+                                            $imageURL = \App\Models\Product::where('id','=', $item['id'])->first()->image;
+                                            $product = \App\Models\Product::where('id','=', $item['id'])->first();
+                                        @endphp
+                                
+                                                <tr class="align-middle">
+                                                    <td>
+                                                        <a href="{{route('showProduct', ['product' => $product])}}">
+                                                            <img height="100" src="{{$imageURL}}" alt="">
+                                                        </a>
+                                                    </td>
+                                                    <td>
+                                                        <a class="text-decoration-none text-dark" href="{{route('showProduct', ['product' => $product])}}">
+                                                        {{$item['name']}}
+                                                        </a>
+                                                    </td>
+                                                    <td>{{$item['price']}}</td>
+                                                    <td>{{$item['qty']}}</td>
+                                                </tr>
+                                
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </li>
 
                     </ul>
