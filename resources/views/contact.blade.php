7d7a6746c487feb6cba042b7914cee08ba8ca759
Jasraj Singh <elijahbaggins3@gmail.com>
2022-11-30 16:16:58 +0000

    Removed shadow from contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,32 +10,32 @@
 
 @section('content')
 
-<div class="container">
+<div class="container mt-4">
   <form class="container form-control center contactForm rounded-0" id="form-container">
   <div class="container form-control-lg  mt-4">
-    <h1 class = "h1- responsive text-center mx-auto">Get In Touch</h1>
+    <h1 class = "h1- responsive text-center mx-auto page-title">Get In Touch</h1>
     <p class ="text-center responsive mx-auto mb-4"> Enter your details below along with your query and we will contact you back as soon as possible!</p>
     <form action="/action_page.php">
       @csrf
       <div class="mb-4">
         <label for="name">Name:</label>
-        <input type="text" class="form-control" id="name" placeholder="Enter name" name="name" required>
+        <input type="text" class="form-control rounded-0" id="name" placeholder="Enter name" name="name" required>
       </div>
       <div class="mb-4">
         <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
+        <input type="email" class="form-control rounded-0" id="email" placeholder="Enter email" name="email" required>
       </div>
       <div class = "mb-4">
         <label for= "number">Phone number</label>
-        <input type="phone number" class="form-control" id="number" placeholder="Enter phone number" name="number" required>
+        <input type="phone number" class="form-control rounded-0" id="number" placeholder="Enter phone number" name="number" required>
       </div>
       <div class="mb-4">
         <label for ="text area">Comments:</label>
-        <textarea class ="form-control" id="text area" rows= "3"placeholder="Please enter reason for contacting us" required></textarea>
+        <textarea class ="form-control rounded-0" id="text area" rows= "3"placeholder="Please enter reason for contacting us" required></textarea>
       </div>
       <div class ="mb-4">
         <label>How would you like us to contact you?</label>
-        <select name ="contact" class ="form-control" id="dropdown" required>
+        <select name ="contact" class ="form-control rounded-0" id="dropdown" required>
           <option disabled selected="">Choose option</option>
           <option>Email</option>
           <option>Phone</option>
@@ -48,7 +48,7 @@
 
       </div>
       <div class ="container">
-      <button type="submit" class="btn btn-dark">Send</button>
+      <button type="submit" class="btn btn-dark rounded-0 w-25">Send</button>
       </div>
     </form>
   </div>
