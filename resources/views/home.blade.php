4f0217f9e1904f899f0cab670f16849c0f34e9c8
Jasraj Singh <elijahbaggins3@gmail.com>
2022-11-30 16:19:04 +0000

    removed old commented code from home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,40 +2,6 @@
 
 @section('content')
 
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @if (session('warning'))
-                        <div class="alert alert-warning" role="alert">
-                            {{ session('warning') }}
-                        </div>
-                    @endif
-
-                    @if (session('fail'))
-                        <div class="alert alert-danger" role="alert">
-                            {{ session('fail') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>  --}}
-{{-- end dashboard container --}}
-
-{{-- Banner --}}
 
 <div id="banner-carousel" class="homepage-banner-carousel carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
