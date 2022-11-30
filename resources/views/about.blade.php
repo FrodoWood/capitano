326c511afd59193109ecad258ea371aae6cccd49
Jasraj Singh <elijahbaggins3@gmail.com>
2022-11-30 14:21:57 +0000

    Updated about us design

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,131 +3,103 @@
 
 
 @section('content')
-    <h1>About us</h1>
-    <br>
-  
 
-<div class="container">
-<div class="row">
-
-<div class="col">
-
-<img src="{{URL('images/motivation2.jpg')}}"alt="motivation" style= "width: 110%; height:300px"  class="rounded float-left">
-</div>
-<div class="col">
-        <h3 class="text-center" style="font-size:50px">Values</h3>
-       
-    <ul  class="text-center">
-        <li class="list-group-item list-group-item-danger" style="font-size:30px" >Integrity</li> 
-        <li class="list-group-item list-group-item-secondary" style="font-size:30px">Team Spirit</li>
-        <li class="list-group-item list-group-item-danger" style="font-size:30px">Be ethical</li>
-        <li class="list-group-item list-group-item-secondary" style="font-size:30px">Diversity</li>
-        <li class="list-group-item list-group-item-danger" style="font-size:30px">Inclusivity</li>
-    </ul>   
-</div>
-
-<div class="col">
-
-<img src="{{URL('images/motivation3.jpg')}}"  alt="motivation" style= "width: 110%; height:300px"  class="rounded float-right">
-</div>
-</div>
-<br><br>
-
-<div class="row">
-<div class="col">
-    <h2 style="color: red"> Make a difference in children's life. Donate below</h2>
- <a href="https://www.unitedthroughsport.org/how-you-can-help/donate/">
-<img src="{{ URL('images/donate.png')}}" alt="donate" style= "width: 100%; height: 300px"> </a>
-</div>
-<div class="col">
+<h1 class="page-title mb-4 text-center mt-4">About us</h1>
 
 
-<h3  style="font-size:50px">Mission</h3>
-     <p  style="font-size:25px">Our mission is to promote health and physical acitivity amongst young adults by
-        offering a wide range of high quality, comfortable workout clothes at a reasonable price.
-        We strive to improve our competitive position and become the market leader, thus we are
-        constantly strengthening our brand and products. </p>
-</div>
-</div>
+<div class="container w-75"><img src="{{URL('images/banner/we_are_capitano.jpg')}}" class="d-block w-100" alt=""></div>
 
-<br><br>
-
-<div class="row">
-    <div class="col">
-    <h3 style="font-size:50px">Vision</h3>
-     <p style="font-size:28px">Capitano Clothing strives to be the world leader in sustainable fitness clothes. Our vision is to bring people
-        together, enrich lives and enhance people's ambitions globally- making sports wear accessible to young adults. 
-     </p>
-    </div>
-    <div class="col">
-    <img src="{{URL('images/motivation1.jpg')}}"  alt="motivation" style= "width: 90%; height:340px;"  class="rounded float-right">
-</div>
-    
-</div>
-
-<br><br>
-
-
-
-    <h3 style="font-size:50px" class="text-center">Company History</h3>
-    <p style="font-size:20px">Capitano clothing was launched in 2018 from a basement in London, United Kingdom. Today, we have stores all over England. 
-        Capitano has over 10,000 employees. We have sold over 50,000 workout clothes to customers all over the world. </p>
-
-      <p style="font-size: 20px">  Capitano Clothing sources all of its supplies from ethical suppliers. All of our products are enviromentally friendly.<p>
-      <p style="font-size: 20px">  We have collaborated with multiple celebrities namely Serena William and Usain Bolt. Moreover, Capitano Clothing has teamed up with award-winning designers Molly Goddard and Regina Geller 
-        to launch collections. </p>
-    <br><br>
-
-    <div class="card text-white bg-secondary mb-3">
-  <div class="card-body">
-  <div class="container">
-  <div class="row">
-    <div class="col-sm">
-        <h4 style="color:blanchedalmond; text-align:center; size:50px">£2,000,000</h4>
-      <p style="text-align:center">Revenue</p>
-    </div>
-    <div class="col-sm">
-    <h4 style="color:blanchedalmond; text-align:center; size:50px">45%</h4>
-      <p style="text-align:center">UK Market Share</p>
-    </div>
-    <div class="col-sm">
-    <h4 style="color:blanchedalmond; text-align:center; size:50px">30%</h4>
-      <p style="text-align:center">Global Market Share</p>
-    </div>
-    <div class="col-sm">
-    <h4 style="color:blanchedalmond; text-align:center; size:50px">95%</h4>
-      <p style="text-align:center">Customer satisfaction rate</p>
-    </div>
-  </div>
-</div>
-            
+    <div class="container w-50 pt-3">
+      {{-- Mission --}}
+      <div class="container my-4 mb-4 pb-4 h5">
+        <div class="row">
+          <div class="col">
+            <h3 class="page-title mb-4 text-center">Mission</h3>
+            <p class="text-justify">Our mission is to promote health and physical acitivity amongst young adults by offering a wide range of high quality, comfortable workout clothes at a reasonable price. We strive to improve our competitive position and become the market leader, thus we are constantly strengthening our brand and products. </p>
+          </div>
         </div>
+      </div>
+      {{-- Vision --}}
+      <div class="container my-4 mb-4 pb-4 h5">
+        <div class="row">
+          <div class="col">
+            <h3 class="page-title mb-4 text-center">Vision</h3>
+            <p class="text-justify">Capitano Clothing strives to be the world leader in sustainable fitness clothes. Our vision is to bring people together, enrich lives and enhance people's ambitions globally- making sports wear accessible to young adults.
+            </p>
+          </div>
+        </div>
+      </div>
         
-     
-    </div>
+      {{-- Values --}}
+      <div class="container mt-4 mb-4 pb-4">
+        <div class="row">
+        <div class="col">
+            <h3 class="text-center page-title mb-4">Values</h3>
+          <ul  class="list-group list-group-flush text-center h5">
+              <li class="list-group-item" >Integrity</li>
+              <li class="list-group-item">Team Spirit</li>
+              <li class="list-group-item">Be ethical</li>
+              <li class="list-group-item">Diversity</li>
+              <li class="list-group-item">Inclusivity</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+
+      
+        <div class="container mt-4 h5">
+      <div class="row">
+        <div class="col">
+          <h3  class="text-center page-title mb-4">Company History</h3>
+          <p class="text-justify">Capitano clothing was launched in 2018 from a basement in London, United Kingdom. Today, we have stores all over England.
+          Capitano has over 10,000 employees. We have sold over 50,000 workout clothes to customers all over the world. </p>
+          <p class="text-justify">  Capitano Clothing sources all of its supplies from ethical suppliers. All of our products are enviromentally friendly.<p>
+          <p class="text-justify">  We have collaborated with multiple celebrities namely Serena William and Usain Bolt. Moreover, Capitano Clothing has teamed up with award-winning designers Molly Goddard and Regina Geller
+            to launch collections. </p>
+        </div>
+      </div>
+        </div>
+
+    <hr class="mt-3 mb-5 pb-4">
+
+
+  {{-- Donate --}}
+  <div class="container text-center">
+    <h2 class="text-center pb-3"> Make a difference in children's life. Donate below</h2>
+    <a href="https://www.unitedthroughsport.org/how-you-can-help/donate/">
+      <button class="btn btn-danger rounded-0" type="button"><span class="h3">Donate</span></button>
+    </a>
   </div>
-</div>
-<br><br>
 
-    <h3 class="text-center" style="font-size:50px">Our Founders</h3>
-    <div class="container">
-     <div class="row">
-     <div class="col-sm">
-     <img  class="rounded-circle" src="{{url('images/founder.jpg')}}" alt="our founder" style= "width: 60%; height:200px" >
-    <p style="font-size: 25px">Emily Rodriguez</p>
+  <hr class="mt-4 mb-5 pb-4">
+
+    <div class="card text-white bg-secondary mb-3 rounded-0">
+      <div class="card-body bg-secondary">
+      <div class="container">
+      <div class="row">
+          <div class="col-lg-3">
+            <h4 class="text-center">£2,000,000</h4>
+            <p class="text-center">Revenue</p>
+          </div>
+          <div class="col-lg-3">
+          <h4 class="text-center">45%</h4>
+            <p class="text-center">UK Market Share</p>
+          </div>
+          <div class="col-lg-3">
+          <h4 class="text-center">30%</h4>
+            <p class="text-center">Global Market Share</p>
+          </div>
+          <div class="col-lg-3">
+            <h4 class="text-center">95%</h4>
+            <p class="text-center">Customer satisfaction rate</p>
+        </div>
+      </div>
+      </div>      
+      </div>
     </div>
-   <div class="col-sm">
-    <img class="rounded-circle"  src="{{url('images/founder1.jpg')}}" alt="our founder" style= "width: 60%; height:200px">
-    <p style="font-size: 25px">Fabian Louise<p>
-   </div>
-
-   <div class="col-sm">
-    <img class="rounded-circle"  src="{{url('images/founder3.jpg')}}" alt="our founder" style= "width: 60%; height:200px">
-    <p style="font-size: 25px">Palomah Greene</p>
-   </div>
-</div>
-    <br> <br>
     
+  </div>
 
-    </div>
+
+    
 @endsection
